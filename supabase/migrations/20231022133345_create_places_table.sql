3df19f37f757755f7dc204c8720c69f5131e4e72
ko-shiro-hap <koushirou.iino@icloud.com>
2023-10-23 00:01:14 +0900

    fix: migrationを修正

--- supabase/migrations/20231022133345_create_places_table.sql
+++ supabase/migrations/20231022133345_create_places_table.sql
@@ -2,13 +2,13 @@
 
 CREATE TABLE places (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  place VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   deleted_at TIMESTAMP
 );
 
-INSERT INTO places (name) VALUES
+INSERT INTO places (place) VALUES
   ('ルーム'),
   ('カフェ'),
   ('屋上'),
--- supabase/migrations/20231022133434_create_working_statuses_table.sql
+++ supabase/migrations/20231022133434_create_working_statuses_table.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS working_statuses;
 
 CREATE TABLE working_statuses (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  status VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   deleted_at TIMESTAMP
 );
 
-INSERT INTO working_statuses (name) VALUES
+INSERT INTO working_statuses (status) VALUES
   ('作業中'),
   ('外出中'),
   ('フリー');
