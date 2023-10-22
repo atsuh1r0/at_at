6d87c75a86fe36251952acd28ec6a38754b2d725
ko-shiro-hap <koushirou.iino@icloud.com>
2023-10-22 23:49:46 +0900

    fix: ステータス取得バグを修正

--- src/services/getUsersWithTodayStatuses.ts
+++ src/services/getUsersWithTodayStatuses.ts
@@ -21,14 +21,18 @@ export const getUsersWithTodayStatuses = async() => {
 
   // usersDataに対応するtodayが今日の日付のstatusesレコードがある場合はそのレコードを追加、ない場合は空の配列を追加
   usersData.forEach((user: any) => {
-    const todayStatus = statusData.filter((status: any) => {
-      return status.date === today && status.user_id === user.id
-    });
-
-    if(todayStatus?.length !== 0) {
-      user.statuses = todayStatus;
-    } else {
+    if(!statusData) {
       user.statuses = [];
+    } else {
+      const todayStatus = statusData.filter((status: any) => {
+        return status.date === today && status.user_id === user.id
+      });
+
+      if(todayStatus?.length !== 0) {
+        user.statuses = todayStatus;
+      } else {
+        user.statuses = [];
+      }
     }
   });
 
