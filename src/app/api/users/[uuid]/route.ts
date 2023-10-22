6da6e7e8fbdd4d5df7a6654110ef77a6ef4ab38a
ko-shiro-hap <koushirou.iino@icloud.com>
2023-10-22 15:35:44 +0900

    add: ログイン中のユーザーを取得する処理を実装

---
+++ src/app/api/users/[uuid]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+const supabase = createClientComponentClient();
+
+export async function GET(request: Request, {params}: {params: {uuid: string}}) {
+  const uuid = params.uuid;
+
+  const { data, error } = await supabase
+  .from('users')
+  .select(`
+    id,
+    name,
+    icon_path,
+    posses(id, posse),
+    generations(id, generation)
+  `)
+  .eq('auth_id', uuid)
+  .is('deleted_at', null);
+
+  return new Response(JSON.stringify({ data: data, error: error}))
+}
